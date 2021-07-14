eec1f613794e0a8ee400f6bc32c485c34a24599f
Qwek-alt <66938425+Qwek-alt@users.noreply.github.com>
2021-07-14 09:28:22 +0800

    Added Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from 'react'
 import './index.css'
-
-import Sidebar from './components/Sidebar'
+import Header from './components/Header'
 
 
 function App() {
+  const [showAddFlashcard, setShowAddFlashcard] = useState(false)
+  const [flashcards, setFlashcards] = useState([
+    {
+      id: 1,
+      question: 'Question 1',
+      answer: 'Answer 1',
+    },
+    {
+      id: 2,
+      question: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam lobortis congue. Nam sit amet sem vitae urna consequat tincidunt. Aenean blandit sapien eu enim viverra fringilla.',
+      answer: 'Ut tincidunt et massa sed semper. Quisque sollicitudin risus in magna sagittis aliquam.',
+    }
+  ])
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
 }
